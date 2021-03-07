f28c65db368189267f34a363ae10277cfdd6c584
Jonathyn Stiverson <jstiverson2002@gmail.com>
2021-03-07 11:48:11 -0600

    Updated the main.cpp for testing.

--- literate-goggles/main.cpp
+++ literate-goggles/main.cpp
@@ -1,35 +1,19 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+#include <vulkan/vulkan.h>
 
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
 
-int main() {
-    glfwInit();
+class HelloTriangleApplication {
+	private:
+		int running;
+	public:
+		HelloTriangleApplication();
+};
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+HelloTriangleApplication::HelloTriangleApplication() {
 
-    std::cout << extensionCount << " extensions supported\n";
+	std::cout << "Hello world!" << std::endl;
 
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
 }
