901893e21a60a73bc60e22db1bc3df73f80ea40e
Jonathyn Stiverson <jstiverson2002@gmail.com>
2021-03-07 13:52:32 -0600

    Added debug mode. Now creates Vulkan instance.

--- literate-goggles/src/HelloTriangleApplication.cpp
+++ literate-goggles/src/HelloTriangleApplication.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
-
-#include "HelloTriangleApplication.h"
-
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan.h>
+#include <iostream>
+#include <vector>
+
+#include "HelloTriangleApplication.h"
 
 
 HelloTriangleApplication::HelloTriangleApplication() {
@@ -29,7 +31,45 @@ void HelloTriangleApplication::initWindow() {
 }
 
 void HelloTriangleApplication::initVulkan() {
+	createInstance();
+};
 
+void HelloTriangleApplication::createInstance() {
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Hello Triangle";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	std::vector<VkExtensionProperties> extensions(extensionCount);
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+	std::cout << "Available extensions:" << std::endl;
+	for (const auto& extension : extensions) {
+		std::cout << "\t" << extension.extensionName << std::endl;
+	}
+
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+	createInfo.enabledLayerCount = 0;
+
+	if ((vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)) {
+		throw std::runtime_error("failed to create instance!");
+	}
 };
 
 void HelloTriangleApplication::mainLoop() {
@@ -41,6 +81,7 @@ void HelloTriangleApplication::mainLoop() {
 };
 
 void HelloTriangleApplication::cleanup() {
+	vkDestroyInstance(instance, nullptr);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 };
--- literate-goggles/src/HelloTriangleApplication.h
+++ literate-goggles/src/HelloTriangleApplication.h
@@ -1,6 +1,7 @@
 #ifndef HelloTriangleApplication_H
 #define HelloTriangleApplication_H
 
+#include <vulkan/vulkan.h>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
@@ -11,11 +12,15 @@
 class HelloTriangleApplication {
 	private:
 		GLFWwindow* window;
+		
 		const uint32_t WIDTH = 800;
 		const uint32_t HEIGHT = 800;
 
+		VkInstance instance;
+
 		void initVulkan();
 		void initWindow();
+		void createInstance();
 		void mainLoop();
 		void cleanup();
 
